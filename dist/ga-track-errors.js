1d6b9614d2d896f300289f9ea10d033bb52cc0ed
Greg Allen <me@jga.me>
2015-03-27 10:38:04 -0700

    Release 0.0.1

--- dist/ga-track-errors.js
+++ dist/ga-track-errors.js
@@ -1,3 +1,121 @@
+/*!
+ * ga-track - Click tracking for Google Analytics
+ * v0.8.0
+ * https://github.com/firstandthird/ga-track
+ * copyright First+Third 2015
+ * MIT License
+*/
+/* global window,_gaq,ga */
+(function($) {
+  $.gaTrack = function(category, action, label) {
+    if ($.gaTrack.debug) {
+      return console.log('GA TRACK', category, action, label);
+    }
+    if (typeof window._gaq === 'undefined' && typeof window.ga === 'undefined') {
+      return this;
+    }
+
+    if(typeof window._gaq !== 'undefined') {
+      _gaq.push(['_trackEvent', category, action, label, null, false]);
+    } else {
+      ga('send', 'event', category, action, label);
+    }
+  };
+
+  $.gaTrack.debug = false;
+
+  $.gaTrackScroll = function() {
+    var $body = $('body');
+    var scrollPercent = 0;
+    var lastPos = 0;
+    var scrollTriggers = {
+      'scroll': false,
+      '25': false,
+      '50': false,
+      '75': false,
+      '100': false
+    };
+
+    var scrollCheck = function() {
+      scrollPercent = ~~(5*Math.round((window.scrollY/($body.height()-window.innerHeight)*100)/5));
+
+      if (lastPos !== scrollPercent) {
+        lastPos = scrollPercent;
+      }
+
+      if (!scrollTriggers.scroll) {
+        scrollTriggers.scroll = true;
+        $.gaTrack('scroll', document.location.toString(), 'Scrolled');
+      }
+
+      switch(scrollPercent) {
+        case 25:
+          if (scrollTriggers['25']) break;
+          $.gaTrack('scroll', document.location.toString(), 'Scrolled 25%');
+          scrollTriggers['25'] = true;
+          break;
+        case 50:
+          if (scrollTriggers['50']) break;
+          $.gaTrack('scroll', document.location.toString(), 'Scrolled 50%');
+          scrollTriggers['50'] = true;
+          break;
+        case 75:
+          if (scrollTriggers['75']) break;
+          $.gaTrack('scroll', document.location.toString(), 'Scrolled 75%');
+          scrollTriggers['75'] = true;
+          break;
+        case 100:
+          if (scrollTriggers['100']) break;
+          $.gaTrack('scroll', document.location.toString(), 'Scrolled 100%');
+          scrollTriggers['100'] = true;
+          break;
+      }
+    };
+
+    $(window).on('scroll', scrollCheck);
+  };
+
+  $.fn.gaTrack = function(options) {
+    var opt = $.extend({}, $.gaTrack.defaults, options);
+
+    return this.each(function() {
+
+      var el = $(this);
+
+      var href = el.data('ga-track-href') || el.attr('href');
+      var target = el.attr('target');
+
+      var cat = el.data('ga-track') || opt.category || 'ga-track';
+      var label = el.data('ga-track-label') || opt.label || href;
+      var action = el.data('ga-track-action') || opt.action || el.text();
+
+      el.on('click', function(e) {
+        $.gaTrack(cat, action, label);
+        if (el.data('ga-track-href') === false) {
+          e.preventDefault();
+        } else if (href && !e.metaKey && e.which === 1 && target != '_blank') {
+          e.preventDefault();
+          setTimeout(function() {
+            window.location = href;
+          }, opt.delay);
+        }
+      });
+
+    });
+  };
+
+  $.gaTrack.defaults = {
+    delay: 200
+  };
+
+  //data-api
+  $(function() {
+    $('[data-ga-track]').gaTrack();
+  });
+
+
+})(jQuery);
+
 ;(function($) {
 
   var initTracking = function() {
@@ -6,23 +124,24 @@
     }
 
     window.addEventListener('error', function(err) {
+      var filename = err.filename ? 'filename: ' + err.filename : '';
       var lineAndColumnInfo = err.colno ? ' line:' + err.lineno + ', column:' + err.colno : ' line:' + err.lineno;
 
-      $.gaTrack('JavaScript Error', err.message, lineAndColumnInfo + ' -> ' + navigator.userAgent);
+      $.gaTrack('JavaScript Error', err.message, filename + lineAndColumnInfo + ' -> ' + navigator.userAgent);
     });
 
-    $.error(function(message) {
+    $.error = function(message) {
       $.gaTrack('jQuery Error', message, navigator.userAgent);
-    });
+    };
 
     $(document).ajaxError(function(event, request, settings) {
       $.gaTrack('jQuery Ajax Error', settings.url, JSON.stringify({
-            result: event.result,
-            status: request.status,
-            statusText: request.statusText,
-            crossDomain: settings.crossDomain,
-            dataType: settings.dataType
-        }));
+        result: event.result,
+        status: request.status,
+        statusText: request.statusText,
+        crossDomain: settings.crossDomain,
+        dataType: settings.dataType
+      }));
     });
   };
 
